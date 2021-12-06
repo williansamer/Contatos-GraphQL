1bbd248865d46a985f128f90a63969f18356ece1
williansamer <willian.samer@gmail.com>
2021-12-06 14:56:59 -0300

    Refatorando e introduzindo o parâmetro context

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-### CRIANDO UM SISTEMA DE CONTATOS EM GRAPHQL
+# CRIANDO UM SISTEMA DE CONTATOS EM GRAPHQL
 
 ## Linguagens/DB/tecnologias usadas:
 > GraphQL
@@ -6,11 +6,11 @@
 > Postgres
 > Docker
 
-## Instalações iniciais 
+### Instalações iniciais 
 > npm init -y
 > npm install nodemon apollo-server graphql graphql-tools/load-files graphql-tools/merge knex pg
 > para rodar o docker, utilizei o script: docker run --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=docker -d postgres
 > npx knex init(Gera um arquivo knexfile.js que só será executado) - terá que fazer algumas atualizações
 > npx knex migrate:make create_table_contato(Irá criar o arquivo migrate)
 
-# Para executar a migrate: npx knex migrate:latest
+### Para executar a migrate: npx knex migrate:latest
--- src/graphql/modules/Contatos/Resolvers.js
+++ src/graphql/modules/Contatos/Resolvers.js
@@ -1,32 +1,31 @@
-const db = require("../../../db")
 
 module.exports = {
-  Query: {
-    contatos: ()=>{
-      return db("contato")
+  Query: {//O que é cada parametro está abaixo.
+    contatos: async(obj,args,{handle})=>{ //O handle é um contexto que vem do apollo server e este contexto esta sendo desestruturado para pegar o handle.
+      return await handle.contatos();
     }
   },
 
   Mutation: {
-    criarContato: async (_, {data})=>{
-      return await (await db("contato").insert(data).returning("*"))[0]
+    criarContato: async (_, {data}, {handle})=>{
+      return await handle.criarContato(data);
     },
 
-    atualizarContato: async (_, {id, data})=>{
-      return await (await db("contato").where({id: id}).update(data).returning("*"))[0]
+    atualizarContato: async (_, {id, data}, {handle})=>{
+      return await handle.atualizarContato(id, data);
     },
 
-    deletarContato: async (_, {filtro: {id, email}}) =>{
-      if(id){
-        return await(await db("contato").where({id: id}).delete())
-      }
-      if(email){
-        return await(await db("contato").where({email: email}).delete())
-      }
-
-      throw new Error("Digite ao menos um parâmetro")
+    deletarContato: async (_, {filtro: {id, email}}, {handle}) =>{
+      return await handle.deletarContato(id, email);
     }
   }
-
-
 }
+
+
+/* 
+Referente aos parâmentos.. 
+OBJ: Obj passa por todas as funções do GraphQL e caso seja necessário, pode ser utilizado para tratar algumas informações.
+ARGS: argumentos passados dos tipos(Query, Mutation, Subscription) para os resolvers
+CONTEXT: contexto do servidor. Passa dados como, por exemplo, o usuário logado, o token, authenticação, etc.
+INFO: Info passa informações adversas como, por exemplo, o erro, o sucesso, etc.
+*/
